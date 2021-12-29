601b051cd3bca769abb831bea72eb29c621dac59
Алексей Бормотов <bormoleshka@gmail.com>
2021-12-29 20:56:09 +0300

    write array

--- Program.cs
+++ Program.cs
@@ -3,6 +3,16 @@
            int count=0;
            
             int [] arr = new int[15];//создали массив
+             
+            
+            for(int i=0; i<arr.Length;i++)
+            {
+                arr[i] = rand.Next(1, 50);
+                Console.Write(arr[i] + " ");
+                    if(arr[i]%2==0)
+                        count++;
+            }
+            Console.WriteLine();//выводим получившийся массив
            
             
             
