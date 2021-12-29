0ae32d9437f90631ed8a9f59b544b5b63f7f9528
Алексей Бормотов <bormoleshka@gmail.com>
2021-12-29 20:52:43 +0300

    create array

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+            Random rand = new Random();
+           int count=0;
+           
+            int [] arr = new int[15];//создали массив
+           
+            
+            
+           
